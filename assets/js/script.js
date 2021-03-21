6051a2bb3d66a6d1cd3f54f1663384cf099d3c29
Potears <ryan.potere@gmail.com>
2021-03-21 11:37:07 -0400

    task saves to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,14 @@ var currentDay = $("#currentDay");
 
 currentDay.text (moment().format("dddd MMMM Do, YYYY"));
 
+//when save button is clicked, save tasks to local storage
+
+$('.saveBtn').on('click',function() {
+    var task = $(this).siblings('.task').val();
+    var time = $(this).parent().attr('id');
+//save in local storage -- task, time
+     localStorage.setItem(time,task); 
+})
+
 })
 
