94a6e2cc4495229897af24d5fa95dc7de4ac440c
Potears <ryan.potere@gmail.com>
2021-03-23 23:08:23 -0400

    read me updated, comments updated, minor fixes to final commit.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # daily-work-schedule
 
+This application allows the user to input their busy schedule which will save to local storage. This will allow the user to refresh the screen and their schedule will re-load on the screen.
+
+![photo](images/screen-shot.png)
+
+When hover the text area it displays a border so the user knows they can click in the text box.
+
+![photo](images/screen-shot-hoover.png)
+
+when you select a text area, the box comes into focus so the user knows which hour they are currently typing in. 
+
+![photo](images/screen-shot-focus.png)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
 //add moment to html
 
-var currentDay = $("#currentDay");
+var currentDay = $('#currentDay');
 
-currentDay.text (moment().format("dddd MMMM Do, YYYY"));
+currentDay.text (moment().format('dddd MMMM Do, YYYY'));
 
 //when save button is clicked, save tasks to local storage
 
@@ -17,25 +17,28 @@ $('.saveBtn').on('click',function() {
 })
 
 
-$('#hour-09 .task').val(localStorage.getItem("hour-09"));
-$('#hour-10 .task').val(localStorage.getItem("hour-10"));
-$('#hour-11 .task').val(localStorage.getItem("hour-11"));
-$('#hour-12 .task').val(localStorage.getItem("hour-12"));
-$('#hour-13 .task').val(localStorage.getItem("hour-13"));
-$('#hour-14 .task').val(localStorage.getItem("hour-14"));
-$('#hour-15 .task').val(localStorage.getItem("hour-15"));
-$('#hour-16 .task').val(localStorage.getItem("hour-16"));
-$('#hour-17 .task').val(localStorage.getItem("hour-17"));
+$('#hour-09 .task').val(localStorage.getItem('hour-09'));
+$('#hour-10 .task').val(localStorage.getItem('hour-10'));
+$('#hour-11 .task').val(localStorage.getItem('hour-11'));
+$('#hour-12 .task').val(localStorage.getItem('hour-12'));
+$('#hour-13 .task').val(localStorage.getItem('hour-13'));
+$('#hour-14 .task').val(localStorage.getItem('hour-14'));
+$('#hour-15 .task').val(localStorage.getItem('hour-15'));
+$('#hour-16 .task').val(localStorage.getItem('hour-16'));
+$('#hour-17 .task').val(localStorage.getItem('hour-17'));
 
 //show colors based on time of day.
-var taskArea = $(".task")
+var taskArea = $('.task')
 
+
+//transform time into interger for comparison with hour ID tag
 function changeColor() {
     var currentTime = parseInt(moment().format('H'));
 
     $(taskArea).each(function(){
-        //for every time-block class
-        var workList = parseInt($(this).attr('id').split('-')[1]);
+        //for each id of taskArea, transform it into an integer for comparison with moment().
+
+        var workList = parseInt($(this).attr('id').split('-')[1]); //splitting the id at the '-' and selecting the second string, then turning into an integer. 
 
         //console log worklist and currentime to ensure they both show as intergers.
         console.log(workList)
@@ -43,14 +46,13 @@ function changeColor() {
         console.log(currentTime)
 
         if (currentTime > workList) {
-            $(this).addClass('past')
+            $(this).addClass('past') //condition to color task blocks grey.
         } 
         if (currentTime === workList) {
-            $(this).addClass('present')
-
+            $(this).addClass('present')//condition to color task blocks red.
         }
 
-        if (currentTime < workList) {$(this).addClass('future')}
+        if (currentTime < workList) {$(this).addClass('future')}//condition to color task blocks green.
 
     })
 
